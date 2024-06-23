8607652a4bb40a9d0f19257188eb5a1fbf4c9fd9
Administrator <mechatroics@gmail.com>
2024-06-24 01:30:35 +0300

    implement 'delete' Action method

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ class ToDoApp(ToDos):
         st.write('This app is to increase your productivity')
         self.todos = self.read_file()
 
+
     def add_todo(self):
         todo = st.session_state['new_todo']
         print(todo)
@@ -16,19 +17,18 @@ class ToDoApp(ToDos):
         self.open_file()
 
     def delete_method(self):
-        # self.todos = self.read_file()
-        to_delete = st.session_state['action']
         for todo in self.todos:
-            if st.checkbox(todo):
-                self.todos.remove(to_delete)
-                st.experimental_rerun()
+            if st.checkbox(todo, key=todo):
+                self.todos.remove(todo)
+                self.open_file()
+                st.experimental_rerun()  # Refresh the page
 
     def action(self):
-        # self.delete_method()
-        st.checkbox(label='',on_change=self.delete_method,value=False,key='action')
-        text = st.text_input(label='',placeholder='Add to do', on_change=self.add_todo, key='new_todo')
+        self.delete_method()
 
-
+        st.text_input(label='', placeholder='Add to do',
+                      on_change=self.add_todo,
+                      key='new_todo')
 
 
 if __name__=='__main__':
